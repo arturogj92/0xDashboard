e8b7969c282b7b22510e258c5839f37394257ad6
arturogj92 <arturogarciaasir@gmail.com>
2025-06-06 21:54:20 +0200

    test with build

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,19 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: ['./vitest.setup.ts']
+    setupFiles: ['./vitest.setup.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        '.next/',
+        'coverage/',
+        '**/*.d.ts',
+        '**/*.config.*',
+        '**/next-env.d.ts'
+      ]
+    }
   },
 
   resolve: {
